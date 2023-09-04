faa75a9615d653e021f078d8ca4d1b2c7a14902b
anhtudo97 <anhtudo97@gmail.com>
2023-09-05 00:08:03 +0700

    feat: add mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
-import { Typography } from '@mui/material'
-import Button from '@mui/material/Button'
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-function App() {
+import { useColorScheme } from '@mui/material/styles';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import { Box } from '@mui/material';
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const mode = event.target.value;
+    setMode(mode);
+  };
 
   return (
-    <>
-      <Typography variant='h1' color="text.secondary">Demo text</Typography>
-    </>
-  )
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box display="flex" alignItems={'center'} gap={1}>
+            <LightModeIcon fontSize="small" />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box display="flex" alignItems={'center'} gap={1}>
+            <DarkModeIcon fontSize="small" />
+            System
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box display="flex" alignItems={'center'} gap={1}>
+            <SettingsBrightnessIcon fontSize="small" />
+            Dark
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
 }
 
-export default App
+function App() {
+  return <ModeSelect />;
+}
+
+export default App;
